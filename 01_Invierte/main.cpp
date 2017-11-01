fb3ad576e3dbeb4e5298b4a927b922e75e8b6232
NeloGino <nelogino@gmail.com>
2017-11-01 00:13:10 -0300

    Invierte funciona correctamente

--- 01_Invierte/main.cpp
+++ 01_Invierte/main.cpp
@@ -2,7 +2,18 @@
 #include "../Lista/Lista.h"
 #include "fnInvierte.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Ejercicio 02/01\n" << std::endl;
+    Lista<int> *lista = new Lista<int>();
+    cout << "HOLA\n";
+    for (int i = 0; i < 10; ++i) {
+        lista->insertarUltimo(i);
+    }
+    fnInvierte(lista);
+
+    for (int j = 0; j < lista->getTamanio(); ++j) {
+        cout << lista->getDato(j) << " ";
+    }
     return 0;
 }
